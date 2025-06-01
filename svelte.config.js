9a42a95f896a093c03262fa8ac2a6d3508ac114a
msas001 <24100062@lums.edu.pk>
2025-06-01 18:16:59 -0400

    trying to add gh pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,8 +2,16 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  preprocess: vitePreprocess(),
+  kit: { 
+    adapter: adapter({
+      pages: 'build',
+      assets: 'build',
+      fallback: '404.html',
+      precompress: false,
+      strict: true
+    })
+  }
 };
 
 export default config;
